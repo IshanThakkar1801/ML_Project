531e9469bbf200a2f0a275cf2d3b419a24e57251
Ishan Thakkar <ishan.1801@gmail.com>
2026-05-26 16:21:00 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -6,7 +6,9 @@ from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import PredictPipeline,CustomData
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+app = application
 
 @app.route('/')
 def index():
